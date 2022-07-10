759daded43cb6ccf5ee6b1d2740d6557b041c39f
Atlas1323 <jbcharvey55@gmail.com>
2022-07-10 18:45:00 -0400

    Copied as appropriate constructors and assignments to Heap

--- src/Structures/Contender.h
+++ src/Structures/Contender.h
@@ -18,33 +18,52 @@ class Contender {
 public:
     static std::vector<Point> Points;
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Constructors/Destructors ----------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     Contender();
-    Contender(const int& size, const Node *nodes);
-    Contender(const Contender& that);
-    Contender(Contender&& that) noexcept;
+
+    Contender(const int &size, const Node *nodes);
+
+    Contender(const Contender &that);
+
+    Contender(Contender &&that) noexcept;
+
     ~Contender();
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Getters/Setters -------------------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     int getSize() const;
+
     void setSize(int size);
 
     Node *getNodes() const;
+
     void setNodes(Node *nodes);
-    void setNode(Node& node, int index);
+
+    void setNode(Node &node, int index);
 
     double getFitness() const;
 
     // Logging/Display methods
     std::string getEqString();
+
     std::string LogString();
+
     void treePrint();
 
     // Static variable getters/setters
     static int getEvalCount();
+
     static void ResetEvaluationCount();
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Member functions ------------------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     double EqParser(int index, const double &x);
     // std::function<double (double)> parseFun(int index);
 
@@ -55,7 +74,9 @@ public:
 
     void randy(const int &index);
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Optomization tools ----------------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
 
     // Spot Mutations
     void valueMutate(const int &index);
@@ -79,7 +100,9 @@ public:
 
     // GP Tools - Pruning
 
-    // Overloaded operators
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+    // Overloaded Operators --------------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     bool operator<(const Contender &that) const;
 
     Contender &operator=(const Contender &that);
@@ -88,29 +111,44 @@ public:
 
 
 private:
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Private Member Elements -----------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     int size_;
-    Node* nodes_;
+    Node *nodes_;
     double fitness_;
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Private Member Functions ----------------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     std::string buildEqString_(int index);
+
     std::string prettyString_(int index);
 
     // Heap Management
     void growHeap_(const int &growFactor);
 
     void swapBranch_(const int &index);
-    void clipBranch_(const int& index);
-    void graftBranch_(const int& index, const int& branch_size, const Node * branch);
-    std::tuple<int, Node*> getBranch_( const int& index);
 
+    void clipBranch_(const int &index);
+
+    void graftBranch_(const int &index, const int &branch_size, const Node *branch);
+
+    std::tuple<int, Node *> getBranch_(const int &index);
+
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Private Static Member Elements ----------------------------------------------------------------------------------
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
     static std::random_device rand_dev_;
     static std::mt19937 rng_;
     static std::uniform_int_distribution<> coin_flip_;
     static int evaluations_;
 
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
     // Private Static Member functions ---------------------------------------------------------------------------------
-    static int growthSize_(const int& depth);
+    // █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+    static int growthSize_(const int &depth);
 };
--- src/Structures/heap.cpp
+++ src/Structures/heap.cpp
@@ -3,3 +3,141 @@
 //
 
 #include "heap.h"
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Constructors/Destructors ----------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+/**
+ * @brief Default constructor
+ */
+Heap::Heap() {
+    size_ = 2;
+    nodes_ = new Node[size_];
+}
+
+/**
+ * @brief Constructor for predefined Nodes*
+ *
+ * @param size
+ * @param nodes
+ */
+Heap::Heap(const int &size, const Node *nodes) {
+    size_ = size;
+    nodes_ = new Node[size_];
+    std::copy(nodes, nodes + size, this->nodes_);
+}
+
+/**
+ * @brief Copy Constructor
+ * @param that
+ */
+Heap::Heap(const Heap &that) {
+    this->size_ = that.size_;
+    this->nodes_ = new Node[that.size_];
+    std::copy(that.nodes_, that.nodes_ + that.size_, this->nodes_);
+}
+
+/**
+ * @brief Move Constructor
+ * @param that
+ */
+Heap::Heap(Heap &&that) noexcept {
+    this->size_ = that.size_;
+    that.size_ = 0;
+
+    this->nodes_ = that.nodes_;
+    that.nodes_ = nullptr;
+}
+
+/**
+ * @brief Default Destructor
+ */
+Heap::~Heap() {
+    delete[] nodes_;
+}
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Public Methods -------------------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+void Heap::clipBranch(const int &index) {
+
+}
+
+void Heap::graftBranch(const int &index, const Heap &branch) {
+
+}
+
+Heap Heap::getBranch(const int &index) {
+
+}
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Getters/Setters ------------------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+int Heap::getSize() const {
+    return size_;
+}
+
+void Heap::setNode(const Node &node, const int &index) {
+    nodes_[index] = node;
+}
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Overloaded Operators -------------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+
+/**
+ * @brief Copy Assignment
+ *
+ * @param that
+ * @return
+ */
+Heap &Heap::operator=(const Heap &that) {
+    if (this != &that) {
+        delete[] this->nodes_;
+
+        this->size_ = that.size_;
+
+        if (that.size_ > 0) {
+            this->nodes_ = new Node[that.size_];
+            // for(int i = 0; i < that.size_; i++)
+            //     this->nodes_[i] = that.nodes_[i];
+            std::copy(that.nodes_, that.nodes_ + that.size_, this->nodes_);
+        } else {
+            this->nodes_ = nullptr;
+        }
+    }
+    return *this;
+}
+
+Heap &Heap::operator=(Heap &&that) noexcept {
+    if (this != &that) {
+        delete[] this->nodes_;
+        this->nodes_ = that.nodes_;
+
+        this->size_ = that.size_;
+
+        that.size_ = 0;
+        that.nodes_ = nullptr;
+    }
+
+    return *this;
+}
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Private Member Variables ---------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+int size_;
+Node *nodes_;
+
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+// Private Methods -------------------------------------------------------------------------------------------------
+// █████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+void growHeap_(const int &growFactor);
+
+void trimHeap_();
+
+
