47df677134d41d428db8e0947e9392c0af2536ab
Atlas1323 <jbcharvey55@gmail.com>
2024-03-08 21:20:25 -0500

    feat: adds Experiment class to data_vis.py

--- scripts/data_vis/data_vis.py
+++ scripts/data_vis/data_vis.py
@@ -5,5 +5,15 @@ class Contender:
         self.eq = eq
 
 
+class Experiment:
+    def __init__(self, data_set, run_time, params, method, data_path, contenders):
+        self.data_set = data_set
+        self.run_time = run_time
+        self.params = params
+        self.method = method
+        self.data_path = data_path
+        self.contenders = contenders
+
+
 if __name__ == '__main__':
     print('what\n')
