4ab63436004bb3a8caed8cec1ef433426807c08b
Atlas1323 <jbcharvey55@gmail.com>
2024-03-12 17:37:15 -0400

    feat adds: Cohort class

--- scripts/data_vis/data_vis.py
+++ scripts/data_vis/data_vis.py
@@ -1,3 +1,5 @@
+import sys
+
 class Contender:
     def __init__(self, evals, fitness, eq):
         self.evals = evals
@@ -13,6 +15,28 @@ class Experiment:
         self.method = method
         self.data_path = data_path
         self.contenders = contenders
+
+
+class Cohort:
+    def __init__(self, experiments, data_set, data_path, parameters, method, step_size, data, n):
+        self.experiments = experiments
+        self.data_set = data_set
+        self.data_path = data_path
+        self.parameters = parameters
+        self.method = method
+        self.step_size = step_size
+        self.data = data
+        self.n = n
+
+        def safe_div(a, b):
+            """Performs a safe division of a by b, to return a number if b = 0, but not crash"""
+            if b == 0:
+                if a > 0:
+                    return sys.float_info.max
+                else:
+                    return -1 * sys.float_info.max
+            else:
+                return a / b
 
 
 if __name__ == '__main__':
