98172934e0b2a42c6bd4b812d53c3d5b6d99c120
Atlas1323 <jbcharvey55@gmail.com>
2024-03-15 13:04:07 -0400

    refactor: restructures tests back into one file

--- scripts/visualizations/test.py
+++ scripts/visualizations/test.py
@@ -1,5 +1,6 @@
 import unittest
 from data_vis.data_vis import Experiment
+from data_vis.data_vis import Contender
 
 
 class TestExperiment(unittest.TestCase):
@@ -41,6 +42,39 @@ class TestExperiment(unittest.TestCase):
         self.assertEqual(result.contenders[0].eq[1], "SIN")
         self.assertEqual(result.contenders[0].eq[25], "BLANK")
         self.assertEqual(result.contenders[0].eq[-1], "BLANK")
+
+    def test_parse_values(self):
+        x = 3.0
+        eq1 = ["ROOT", "VAR"]
+
+        eq2 = ["ROOT", "ADD", "VAR", "2.0"]
+        eq3 = ["ROOT", "SUB", "VAR", "2.0"]
+        eq4 = ["ROOT", "MLT", "VAR", "2.0"]
+        eq5 = ["ROOT", "DIV", "VAR", "2.0"]
+        eq6 = ["ROOT", "ADD", "VAR", "2.0"]
+
+
+class TestContender(unittest.TestCase):
+    def test_read_in_contender(self):
+        """
+        Test if read_in_contender(line) creates a stable
+        :return:
+        """
+        a = ("250 | 0.507485 | ROOT SIN SIN BLANK SUB BLANK BLANK BLANK COS DIV BLANK BLANK BLANK BLANK BLANK BLANK "
+             "SUB BLANK ADD MLT BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK 8.583254 VAR "
+             "BLANK BLANK -5.630509 VAR VAR VAR BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK "
+             "BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK "
+             "BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK "
+             "BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK "
+             "BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK "
+             "BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK BLANK\n")
+        contender = Contender.read_in_contender(a)
+        self.assertEqual(contender.evals, 250)
+        self.assertEqual(contender.fitness, 0.507485)
+        self.assertEqual(contender.eq[0], "ROOT")
+        self.assertEqual(contender.eq[1], "SIN")
+        self.assertEqual(contender.eq[25], "BLANK")
+        self.assertEqual(contender.eq[-1], "BLANK")
 
 
 
