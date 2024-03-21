ee1afabc8ce767266968d5fae2565ab27fbabb76
Atlas1323 <jbcharvey55@gmail.com>
2024-03-21 10:46:42 -0400

    docs: updates docs for Point.cpp

--- src/Structures/Point.cpp
+++ src/Structures/Point.cpp
@@ -1,11 +1,22 @@
 #include <Point.h>
 
+/**
+ * @brief default constructor
+ *
+ * Produces point with value (0, 0)
+ */
 Point::Point()
 {
 	this->x = 0;
 	this->y = 0;
 }
 
+/**
+ * @brief Constructor for specified point
+ *
+ * @param x
+ * @param y
+ */
 Point::Point(double x, double y)
 {
 	this->x = x;
